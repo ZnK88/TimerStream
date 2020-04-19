97990c3d6f8ff3f5cb2898ef9bcad769d9baf17d
ZnK <michael.thouvenin@epitech.eu>
2020-04-19 20:19:35 +0200

    Push v1

---
+++ script.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+    const queryString = window.location.search;
+    console.log(queryString)
+    const urlParams = new URLSearchParams(queryString);
+    heures = urlParams.getAll('heures')
+    minutes = urlParams.getAll('minutes')
+    secondes = urlParams.getAll('secondes')
+    console.log(urlParams)
+    console.log(window.location.protocol + "//" + window.location.host)
+    if(urlParams != "")
+    {
+        $("body").empty();
+        // valeurMinute = parseInt($('#minute').val())
+        // valeurSeconde = parseInt($('#seconde option:selected').val())
+        timer(heures,minutes,secondes)
+    }
+    console.log($('#urlGenerated').length);
+    $("#valider").on('click',function(){
+        valeurHeure = parseInt($('#heure option:selected').val())
+        valeurMinute = parseInt($('#minute option:selected').val())
+        valeurSeconde = parseInt($('#seconde option:selected').val())
+        countDownDate = new Date();
+        // timer(valeurMinute,valeurSeconde);
+        // countDownDate.setHours(countDownDate.getHours() + valeurHeure);
+        // countDownDate.setMinutes(countDownDate.getMinutes() + valeurMinute);
+        // countDownDate.setSeconds(countDownDate.getSeconds() + valeurSeconde);
+        // Interval = setInterval(function(){
+        //     // countDownDate = new Date();
+        //     // console.log(typeof valeurMinute)
+        //     // console.log(typeof valeurSeconde)
+        //     // countDownDate.setMinutes(countDownDate.getMinutes() + valeurMinute);
+        //     // countDownDate.setSeconds(countDownDate.getSeconds() + valeurSeconde);
+        //     // alert(countDownDate.getMinutes());
+        //     var now = new Date();
+        //     var distance = countDownDate - now;
+        //     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        //     total = (days + hours + minutes + seconds);
+        //     console.log(total);
+        //     console.log(days + "d " + hours + "h "
+        //     + minutes + "m " + seconds + "s ");
+        //     $("body").empty();
+        //     $("body").html(days + "d " + hours + "h "
+        //     + minutes + "m " + seconds + "s ").css("font-size","50px").css("background-color","rgba(0, 0, 0, 0)");
+        //     if(total <= 0)
+        //     {
+        //         clearInterval(Interval)
+        //         $("body").empty();
+        //     }
+        // }, 500);
+        url = window.location.protocol + "//" + window.location.host + window.location.pathname 
+        +"?heures="+valeurHeure+"&minutes="+valeurMinute +"&secondes="+valeurSeconde
+        if($('#urlGenerated').length == 0)
+        {
+            $('#createTimer').after("<textarea id='urlGenerated' readonly='readonly'>")
+            $('#createTimer').after("<p>Reglage conseillé : Largeur:300 , Hauteur:50</p>")
+            $('#urlGenerated').text()
+            $('#urlGenerated').css("resize","none").css("width","500px")
+            $('#urlGenerated').text(url)
+        }
+        else
+        {
+            $('#urlGenerated').empty()
+            $('#urlGenerated').text(url)
+        }
+        console.log($('#urlGenerated').length);
+
+    })
+
+})
+function timer(heure = 0,minute = 0,seconde = 0)
+{
+    valeurHeure = parseInt(heure)
+    valeurMinute = parseInt(minute)
+    valeurSeconde = parseInt(seconde)
+    countDownDate = new Date();
+    // timer(valeurMinute,valeurSeconde);
+    countDownDate.setHours(countDownDate.getHours() + valeurHeure);
+    countDownDate.setMinutes(countDownDate.getMinutes() + valeurMinute);
+    countDownDate.setSeconds(countDownDate.getSeconds() + valeurSeconde);
+    Interval = setInterval(function(){
+        // countDownDate = new Date();
+        // console.log(typeof valeurMinute)
+        // console.log(typeof valeurSeconde)
+        // countDownDate.setMinutes(countDownDate.getMinutes() + valeurMinute);
+        // countDownDate.setSeconds(countDownDate.getSeconds() + valeurSeconde);
+        // alert(countDownDate.getMinutes());
+        var now = new Date();
+        var distance = countDownDate - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        total = (days + hours + minutes + seconds);
+        console.log(total);
+        console.log(days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ");
+        $("body").empty();
+        $("body").html(days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ").css("font-size","50px").css("background-color","rgba(0, 0, 0, 0)");
+        if(total <= 0)
+        {
+            clearInterval(Interval)
+            $("body").empty();
+        }
+    }, 1000);
+}
